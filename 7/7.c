80a15358f42a24c71ed1e218822ca778a2a4bcd4
Kshitij Dhar <39898582+Dhar15@users.noreply.github.com>
2020-06-11 19:13:37 +0530

    Create 7.c

---
+++ 7/7.c
@@ -0,0 +1,108 @@
+/* Question - Design, develop and implement a C/C++/Java program to simulate the working of
+   Shortest remaining time and Round Robin (RR) scheduling algorithms.
+   Experiment with different quantum sizes for RR algorithm. */
+
+#include<stdio.h>
+typedef struct J
+{
+	int arrival,finish,burst,tat,wt;
+}Job;
+
+void scheduler(Job job[], int n, int q, int c)
+{
+	int bursts[100];
+	for(int i=0;i<n;i++)
+		bursts[i] = job[i].burst;
+		
+	int t=0,done=0,curr,diff;
+	float tat_sum=0,wt_sum=0;
+	
+	if(c==0)
+		curr = -1;
+	else
+		curr = 0;
+		
+	while(done<n)
+	{
+		if(c==1)
+		{
+			for(int x=0;x<n;x++)
+			{
+				if(job[curr].burst == 0)
+					curr = x;
+					
+				if(job[x].burst < job[curr].burst)
+					if(job[x].burst > 0 && job[x].arrival <= t)
+						curr = x;
+			}
+			diff = 1;			//Pre-emptive mode
+			//diff = job[curr].burst	//Non Pre-emptive mode
+		}
+		else
+		{
+			while(1)
+			{
+				curr = (curr+1) % n;
+				if(job[curr].burst != 0)
+					break;
+			}
+			diff = (q<=job[curr].burst) ? q : job[curr].burst;
+		}
+		
+		job[curr].burst -= diff;
+		t += diff;
+		
+		if(job[curr].burst == 0)		//if curr job finished
+		{
+			done++;
+			job[curr].finish = t;
+		}
+	}
+	if(c==1)
+		printf("\nThe SJF schedule details are-\n");
+	else
+		printf("\nThe Round Robin schedule details are-\n");
+		
+	for(int i=0;i<n;i++)
+		job[i].burst = bursts[i];
+	
+	printf("\nJob\tCT\tTAT\tWT\n");
+	for(int i=0;i<n;i++)
+	{
+		job[i].tat = job[i].finish - job[i].arrival;
+		job[i].wt = job[i].tat - job[i].burst;
+		printf("%d\t%d\t%d\t%d\n",i+1,job[i].finish,job[i].tat,job[i].wt);
+		tat_sum += job[i].tat;
+		wt_sum += job[i].wt;
+	}
+	printf("\nAverage Turnaround Time = %f\nAverage Waiting Time = %f\n", tat_sum/n, wt_sum/n);
+} 
+
+int main()
+{
+	Job job[100];
+	int n,q,ch;
+	printf("Enter the no.of jobs:\n");
+	scanf("%d",&n);
+	printf("Enter Arrival and Burst Time:\n");
+	for(int i=0;i<n;i++)
+	{
+		printf("J%d-\t",i+1);
+		scanf("%d %d", &job[i].arrival,&job[i].burst);
+	}
+	printf("1.Round Robin\n2.Shortest Job First\nChoose an option: ");
+	scanf("%d",&ch);
+	
+	switch(ch)
+	{
+		case 1:
+			printf("Enter quantum size for RR: ");
+			scanf("%d",&q);
+			scheduler(job,n,q,0);
+			break;
+		case 2:
+			scheduler(job,n,q,1);
+		default:
+			printf("Invalid Choice\n");
+	}
+}
